ebd83594763c0097557697a26f4a58713d4467f9
Entalizen <danielarya185@gmail.com>
2019-12-22 17:44:00 +0700

    installing request

--- middleware/templater.js
+++ middleware/templater.js
@@ -1,5 +1,5 @@
 module.exports = (request, response, next) => {   
-    response.render = (directory, data) => {
+    response.render = (directory, data = {}) => {
         let template = require(`../public/views/${directory}/index.marko`);
         response.marko(template, data);
     };
