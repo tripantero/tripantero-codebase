d7c9e630282393e4740c44372b2fc7eea3a9f89c
Entalizen <danielarya185@gmail.com>
2019-12-23 00:41:17 +0700

    session model is done

--- service/session.service.js
+++ service/session.service.js
@@ -2,6 +2,7 @@ const {Model, Schema, Optional} = require('./model');
 
 module.exports = new Model('session-database', new Schema({
     username: Optional(String),
+    role: String,
     email: String,
     password: String,
     date: {
