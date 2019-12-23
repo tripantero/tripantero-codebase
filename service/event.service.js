056dadf4893c263e65ae1244351e1705f0c1c76a
Entalizen <danielarya185@gmail.com>
2019-12-23 23:09:52 +0700

    event service is done

--- service/event.service.js
+++ service/event.service.js
@@ -1,6 +1,7 @@
 const {Model, Schema, Optional} = require('./model');
 
-module.exports = new Model('session-database', new Schema({
+module.exports = new Model('event', new Schema({
+    event_author: String,
     title: String,
     description: String,
     image_url: Optional(String),
