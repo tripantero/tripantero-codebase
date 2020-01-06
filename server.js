3068653b6db41498d46419616ce8b0808c2a700e
HotPotatoC <43059506+HotPotatoC@users.noreply.github.com>
2020-01-06 18:53:03 +0700

    Added landing page

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ let isProduction = process.env.NODE_ENV == 'production';
 
 require('lasso').configure({
     "outputDir": "static",
-    "urlPrefix": __dirname +'/public/assets/static',
+    "urlPrefix": '/public/assets/static',
     "minify": isProduction,
     "bundlingEnabled": isProduction,
     "fingerprintsEnabled": isProduction,
