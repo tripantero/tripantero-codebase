5cae8cb0c2810207f5c92f0c123871e127d35b2a
Entalizen <danielarya185@gmail.com>
2020-01-10 23:04:45 +0700

    session-api is done, yes i can guarantee

--- controllers/authentication.post.js
+++ controllers/authentication.post.js
@@ -1,6 +1,8 @@
 const Controller = new (require('./Controller').Controller)('/login', __filename);
 const authService = require('../service/session.service');
 const generator = require('../auxiliary/sessionGenerator');
+const validator = require('../middleware/session-validator');
+Controller.middlewares.push(validator);
 Controller.enableBodyparser();
 
 let functional = (request, response) => {
--- controllers/registration.post.js
+++ controllers/registration.post.js
@@ -1,10 +1,12 @@
 const Controller = new (require('./Controller').Controller)('/register', __filename);
 const authService = require('../service/session.service');
+const validator = require('../middleware/session-validator');
+Controller.middlewares.push(validator);
 Controller.enableBodyparser();
 
 let functional = (request, response) => {
     if(!request.body.password == request.body.password_confirm) {
-
+        response.send("password confirmation fail");
     }
     let records = {
         username: request.body.name,
@@ -14,7 +16,7 @@ let functional = (request, response) => {
         date: {
             registered: new Date(),
             lastLogin: new Date(),
-            lastVisit: new Date()
+            lastLogout: new Date()
         },
         listEventsCreatedId: [],
         listJoinedEventId: [],
