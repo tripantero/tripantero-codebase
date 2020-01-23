620a7480e3d42527184adb3ff365687b324e48af
Entalizen <danielarya185@gmail.com>
2020-01-24 01:21:38 +0700

    yaaz i've fix it

--- controllers/index.get.js
+++ controllers/index.get.js
@@ -1,4 +1,5 @@
 const Controller = new(require('./Controller').Controller)('/', __filename);
+Controller.middlewares.push(require('../middleware/username-templater'));
 
 let functional = (request, response) => {
     response.render("index")
